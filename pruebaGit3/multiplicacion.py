408e4b18e6d6f756de700e2c002e445b2548f910
Samantha-Neyra <samantha.kateryn@gmail.com>
2025-02-28 16:58:01 -0500

    primer  archivos subidos

---
+++ pruebaGit3/multiplicacion.py
@@ -0,0 +1,9 @@
+
+
+def multiplicacion (a , b):
+    return (a*b)
+
+resultado=multiplicacion (2 , 4)
+
+print (resultado)
+
---
+++ pruebaGit3/saludoCont.py
@@ -0,0 +1,7 @@
+nombre = "Ivana Neyra Marquez"
+saludo = "Buenos dias"
+hi = "Hola"
+
+saludoPersonalizado=hi+" "+saludo+" "+nombre
+
+print(saludoPersonalizado)
